ad784d6ea5480e1ae0d2a8c385d95b27b4585a94
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-02-12 00:16:17 +0700

    :heavy_plus_sign: Google Firebase

---
+++ src/core/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCr7d4wJvhXLhcL5ts5V8lFOHGDDyXy__M",
+  authDomain: "cesc-12.firebaseapp.com",
+  databaseURL: "https://cesc-12.firebaseio.com",
+  projectId: "cesc-12",
+  storageBucket: "cesc-12.appspot.com",
+  messagingSenderId: "234372643119"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,39 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import firebase from "./core/firebase";
+
+var provider = new firebase.auth.FacebookAuthProvider();
+
+// firebase.auth().signInWithRedirect(provider);
+
+firebase
+  .auth()
+  .getRedirectResult()
+  .then(function(result) {
+    if (result.credential) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    console.log(result);
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
