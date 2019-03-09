5c36fc998c01f85ee6d66ebd145cff56f711aaeb
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-03-10 00:14:08 +0700

    :bug: Fix not check validation when change other fields

--- src/view/camper_pass.vue
+++ src/view/camper_pass.vue
@@ -1,86 +1,88 @@
 <template>
   <v-card-text>
-    <v-layout row wrap>
-      <v-flex sm7 xs12>
-        <v-text-field
-          v-model="form.camp1"
-          label="ค่ายที่ 1"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-        />
-      </v-flex>
+    <v-form ref="formPass">
+      <v-layout row wrap>
+        <v-flex sm7 xs12>
+          <v-text-field
+            v-model="form.camp1"
+            label="ค่ายที่ 1"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+          />
+        </v-flex>
 
-      <v-flex sm5 xs12>
-        <v-text-field
-          v-model="form.camp1_uni"
-          label="มหาวิทยาลัย / หน่วยงาน"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-          :rules="[rules.required_uni(form.camp1)]"
-        />
-      </v-flex>
+        <v-flex sm5 xs12>
+          <v-text-field
+            v-model="form.camp1_uni"
+            label="มหาวิทยาลัย / หน่วยงาน"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+            :rules="[rules.required_uni(form.camp1)]"
+          />
+        </v-flex>
 
-      <v-flex sm7 xs12>
-        <v-text-field
-          v-model="form.camp2"
-          label="ค่ายที่ 2"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-        />
-      </v-flex>
+        <v-flex sm7 xs12>
+          <v-text-field
+            v-model="form.camp2"
+            label="ค่ายที่ 2"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+          />
+        </v-flex>
 
-      <v-flex sm5 xs12>
-        <v-text-field
-          v-model="form.camp2_uni"
-          label="มหาวิทยาลัย / หน่วยงาน"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-          :rules="[rules.required_uni(form.camp2)]"
-        />
-      </v-flex>
+        <v-flex sm5 xs12>
+          <v-text-field
+            v-model="form.camp2_uni"
+            label="มหาวิทยาลัย / หน่วยงาน"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+            :rules="[rules.required_uni(form.camp2)]"
+          />
+        </v-flex>
 
-      <v-flex sm7 xs12>
-        <v-text-field
-          v-model="form.camp3"
-          label="ค่ายที่ 3"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-        />
-      </v-flex>
+        <v-flex sm7 xs12>
+          <v-text-field
+            v-model="form.camp3"
+            label="ค่ายที่ 3"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+          />
+        </v-flex>
 
-      <v-flex sm5 xs12>
-        <v-text-field
-          v-model="form.camp3_uni"
-          label="มหาวิทยาลัย / หน่วยงาน"
-          hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
-          :clearable="!disable && !readonly"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-          :rules="[rules.required_uni(form.camp3)]"
-        />
-      </v-flex>
+        <v-flex sm5 xs12>
+          <v-text-field
+            v-model="form.camp3_uni"
+            label="มหาวิทยาลัย / หน่วยงาน"
+            hint="ถ้าหากไม่มีไม่จำเป็นต้องกรอก"
+            :clearable="!disable && !readonly"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+            :rules="[rules.required_uni(form.camp3)]"
+          />
+        </v-flex>
 
-      <v-flex xs12>
-        <v-textarea
-          v-model="form.knowFrom"
-          label="รู้จักค่ายนี้มาจากที่ไหน"
-          rows="3"
-          :readonly="readonly"
-          :disabled="disable && !readonly"
-          :rules="[rules.required]"
-        />
-      </v-flex>
-    </v-layout>
+        <v-flex xs12>
+          <v-textarea
+            v-model="form.knowFrom"
+            label="รู้จักค่ายนี้มาจากที่ไหน"
+            rows="3"
+            :readonly="readonly"
+            :disabled="disable && !readonly"
+            :rules="[rules.required]"
+          />
+        </v-flex>
+      </v-layout>
+    </v-form>
   </v-card-text>
 </template>
 
@@ -121,6 +123,7 @@ export default {
     form: {
       handler(val) {
         this.$emit("input", val);
+        this.validate();
       },
       deep: true
     },
@@ -140,6 +143,13 @@ export default {
     this.$emit("input", this.form);
 
     bus.$on("reg.close", () => (this.disable = true));
+  },
+  methods: {
+    validate() {
+      if (this.$refs.formPass.validate()) {
+        this.snackbar = true;
+      }
+    }
   }
 };
 </script>
