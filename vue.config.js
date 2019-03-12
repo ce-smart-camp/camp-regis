9eb7f726b4fccc0f111e65aee4a1201e859e5b23
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-03-12 09:51:09 +0700

    :sparkles: Add service worker

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
   configureWebpack:{
     optimization: {
@@ -5,6 +7,14 @@ module.exports = {
         minSize: 10000,
         maxSize: 250000,
       }
-    }
+    },
+    plugins: [
+      new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast 
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true
+      })
+    ]
   }
 }
