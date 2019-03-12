765fad90e814c547bcbf59de99623d462b8836aa
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-03-12 10:42:05 +0700

    :speech_balloon: Change alert text

--- src/main.js
+++ src/main.js
@@ -9,20 +9,23 @@ import bus from "./core/bus";
 
 Vue.config.productionTip = false;
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
   });
 }
 
-window.addEventListener('offline', function(e) {
+window.addEventListener("offline", function(e) {
   bus.$emit(
     "dialog.on",
-    "ขออภัยน้องๆด้วย เนื่องจากเว็บของพี่ๆอยาจจะยังไม่พร้อมสำหรับการทำงานโดยไม่มีเน็ต จึงทำให้เว็บอาจทำงานไม่ถูกต้อง"
+    "ขออภัยน้องๆด้วย เนื่องจากเว็บของพี่ๆอยาจจะยังไม่พร้อมสำหรับการทำงานโดยไม่มีอินเตอร์เน็ต จึงทำให้เว็บอาจทำงานไม่ถูกต้อง"
   );
 });
 
