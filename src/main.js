6c8fbc034f1c256318e1209806a0fb5e16a58a02
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-03-12 19:26:08 +0700

    :bug: Fix install service worker in localhost

--- src/main.js
+++ src/main.js
@@ -9,20 +9,21 @@ import bus from "./core/bus";
 
 Vue.config.productionTip = false;
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(registration => {
-        console.log("SW registered: ", registration);
-      })
-      .catch(registrationError => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
+if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1")
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(registration => {
+          console.log("SW registered: ", registration);
+        })
+        .catch(registrationError => {
+          console.log("SW registration failed: ", registrationError);
+        });
+    });
+  }
 
-window.addEventListener("offline", function(e) {
+window.addEventListener("offline", function() {
   bus.$emit(
     "dialog.on",
     "ขออภัยน้องๆด้วย เนื่องจากเว็บของพี่ๆอยาจจะยังไม่พร้อมสำหรับการทำงานโดยไม่มีอินเตอร์เน็ต จึงทำให้เว็บอาจทำงานไม่ถูกต้อง"
