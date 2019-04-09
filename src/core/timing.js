1f2b3e5c5e24ab6078ca57ca37e08a3fbf538943
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-04-09 15:51:52 +0700

    :ambulance: Change timeout

--- src/core/timing.js
+++ src/core/timing.js
@@ -1,6 +1,6 @@
 import bus from "./bus";
 
-const tOut1 = new Date(1554829200000); // 10 เมษายน 2019 เวลา 0:00:00
+const tOut1 = new Date(1554915600000); // 11 เมษายน 2019 เวลา 0:00:00
 const tOut2 = new Date(1555693200000); // 20 เมษายน 2019 เวลา 0:00:00
 
 export { tOut1, tOut2 };
